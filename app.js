a58f245844ed05d7f1e723076a04ffed36990c2d
VochanecA <126856875+VochanecA@users.noreply.github.com>
2023-05-29 12:35:36 +0200

    malo eksperimentisanja

--- app.js
+++ app.js
@@ -16,7 +16,18 @@
 //   console.log(__dirname);
 // });
 
+
+
+// moze i u jedan red"
+
+// var path=require("path");var express=require("express");var authRoutes=require("./routes/auth.routes");var app=express();app.set("view engine","ejs");app.set("views",path.join(__dirname,"views"));app.use(authRoutes);var port=3E3;app.listen(port,function(){console.log("Server radi na portu "+port);console.log(__dirname)});
+
+
+
 // ili brze,mnogo brzeeeeee :) 
+
+
+
 // var $jscomp = $jscomp || {};
 // $jscomp.scope = {};
 // $jscomp.createTemplateTagFirstArg = function(a) {
@@ -37,13 +48,6 @@
 // });
 
 
-// ili jos brze:
+// ili jos brze...sve u jedan red:
 
-var a = require("path"), b = require("express"), c = require("./routes/auth.routes"), d = b();
-d.set("view engine", "ejs");
-d.set("views", a.join(__dirname, "views"));
-d.use(c);
-d.listen(3000, function() {
-  console.log("Server radi na portu 3000");
-  console.log(__dirname);
-});
+var $jscomp=$jscomp||{};$jscomp.scope={};$jscomp.createTemplateTagFirstArg=function(a){return a.raw=a};$jscomp.createTemplateTagFirstArgWithRaw=function(a,b){a.raw=b;return a};var path=require("path"),express=require("express"),authRoutes=require("./routes/auth.routes"),app=express();app.set("view engine","ejs");app.set("views",path.join(__dirname,"views"));app.use(authRoutes);var port=3E3;app.listen(port,function(){console.log("Server radi na portu "+port);console.log(__dirname)});
