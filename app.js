a579416117efe8cbea33072d06ea8e89524635e8
VochanecA <126856875+VochanecA@users.noreply.github.com>
2023-06-03 16:44:01 +0200

    OUTPUT PRODUCTS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 const checkAuthStatusMiddleware = require('./middlewares/check-auth');
+const protectRoutesMiddleware = require('./middlewares/protect-routes');
 const authRoutes = require('./routes/auth.routes');
 const productsRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
@@ -20,6 +21,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
+app.use('/products/assets', express.static('product-data'));
 app.use(express.urlencoded({ extended: false }));
 
 const sessionConfig = createSessionConfig();
@@ -30,10 +32,13 @@ app.use(csrf());
 app.use(addCsrfTokenMiddleware);
 app.use(checkAuthStatusMiddleware);
 
+
+
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
-app.use('/admin', adminRoutes); //dodaj parametar za path koji sluzi kao filter
+app.use(protectRoutesMiddleware);
+app.use('/admin', adminRoutes);
 
 app.use(errorHandlerMiddleware);
 
@@ -45,7 +50,6 @@ db.connectToDatabase()
     console.log('Failed to connect to the database!');
     console.log(error);
   });
-
 
 
 // moze i u jedan red --- ovo je pocetak samo"
