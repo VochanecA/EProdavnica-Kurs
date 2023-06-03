931d10711a35920d7043e6b64651a9002588e62c
VochanecA <126856875+VochanecA@users.noreply.github.com>
2023-06-03 12:50:57 +0200

    Start-Finish

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
+const csrf = require('csurf');
 
-const db = require("./data/database");
-const authRoutes = require("./routes/auth.routes");
+const db = require('./data/database');
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(csrf());
+
+app.use(addCsrfTokenMiddleware);
 
 app.use(authRoutes);
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
   .then(function () {
     app.listen(3000);
   })
   .catch(function (error) {
-    console.log("Failed to connect to the database!");
+    console.log('Failed to connect to the database!');
     console.log(error);
   });
 
-
-// moze i u jedan red"
+// moze i u jedan red --- ovo je pocetak samo"
 
 // var path=require("path");var express=require("express");var authRoutes=require("./routes/auth.routes");var app=express();app.set("view engine","ejs");app.set("views",path.join(__dirname,"views"));app.use(authRoutes);var port=3E3;app.listen(port,function(){console.log("Server radi na portu "+port);console.log(__dirname)});
 
