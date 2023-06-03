dd12b8b5183c6246b6fbd4c4b985f13b68deb50d
VochanecA <126856875+VochanecA@users.noreply.github.com>
2023-06-03 15:06:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ db.connectToDatabase()
   });
 
 
+
 // moze i u jedan red --- ovo je pocetak samo"
 
 // var path=require("path");var express=require("express");var authRoutes=require("./routes/auth.routes");var app=express();app.set("view engine","ejs");app.set("views",path.join(__dirname,"views"));app.use(authRoutes);var port=3E3;app.listen(port,function(){console.log("Server radi na portu "+port);console.log(__dirname)});
